34ebbc9fbf126303bafd6b845d91163f80c59473
Cesar Jovany Amel <cesar_jovany@hotmail.com>
2026-08-17 12:56:50 -0600

    add: Se agrega columna en get-route-supervisors
    Se agrega columna mail_institutional

--- class/Families.php
+++ class/Families.php
@@ -1,24 +1,27 @@
 <?php
+
 use App\Models\DataConn;
 
-class Families extends DataConn {   
-
-    private $itemsTable = "families_ykt";      
+class Families extends DataConn
+{
+    private $itemsTable = "families_ykt";
     public $id;
     public $name;
     public $description;
     public $price;
     public $category_id;
-    public $created; 
-    public $modified; 
+    public $created;
+    public $modified;
 
     private $conn;
-    
-    public function __construct(){
+
+    public function __construct()
+    {
         $this->conn = $this->dbConn();
     }
 
-    function getAllFamiliesActives(){
+    public function getAllFamiliesActives()
+    {
         $result = null;
 
         $stmt = $this->conn->prepare("SELECT t1.id_family, t1.family_code, t1.family_name, t1.password
@@ -26,7 +29,7 @@ class Families extends DataConn {
                                       WHERE t1.status = 1");
 
         $stmt->execute();
-                
+
         while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
             $id_family = $row->id_family;
 
@@ -36,9 +39,9 @@ class Families extends DataConn {
             $row->ykt_linea = '';
             $row->update_family_information = 'https://intra-ykt.com/intraschool/ykt-online/external_campaigns/data_update/families/login.php';
             //--- --- ---//
-            
+
             //--- Dirección principal ---//
-            $stmt0 = $this->conn->prepare("SELECT 
+            $stmt0 = $this->conn->prepare("SELECT
                                              CASE
                                                 WHEN t1.int_number != '' THEN CONCAT(t1.street, ' no. ext.', t1.ext_number, ', no. int.', t1.int_number, ', ', t1.colony, ', ', t1.delegation, ', CP. ', t1.postal_code)
                                                 ELSE CONCAT(t1.street, ' no. ext.', t1.ext_number, ', ', t1.colony, ', ', t1.delegation, ', CP. ', t1.postal_code)
@@ -56,9 +59,9 @@ class Families extends DataConn {
             $row->principal_address = $principal_address;
 
             $secondary_contacts = array();
-            
+
             //--- Contactos de confianza ---//
-            $stmt1 = $this->conn->prepare("SELECT t1.trusted_contact_id, t1.contact_full_name AS contact_name, t1.relationship, t1.cell_phone, 
+            $stmt1 = $this->conn->prepare("SELECT t1.trusted_contact_id, t1.contact_full_name AS contact_name, t1.relationship, t1.cell_phone,
                                                 CASE
                                                     WHEN t1.internal_number != '' THEN CONCAT(t1.street, ' no. ext.', t1.external_number, ', no. int.', t1.internal_number, ', ', t1.colony, ', ', t1.delegation, ', CP. ', t1.postal_code)
                                                     ELSE CONCAT(t1.street, ' no. ext.', t1.external_number, ', ', t1.colony, ', ', t1.delegation, ', CP. ', t1.postal_code)
@@ -79,7 +82,8 @@ class Families extends DataConn {
         return $result;
     }
 
-    function getAllDadsActives(){
+    public function getAllDadsActives()
+    {
         $result = null;
 
         $stmt = $this->conn->prepare("SELECT t1.id_family, t1.family_code, CONCAT(t2.lastname, ' ', t2.name) AS father_name, t2.mail, t2.cell_phone, t2.landline,
@@ -90,7 +94,7 @@ class Families extends DataConn {
                                       WHERE t1.status = 1");
 
         $stmt->execute();
-                
+
         while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
             $result[] = $row;
         }
@@ -98,7 +102,8 @@ class Families extends DataConn {
         return $result;
     }
 
-    function getAllMomsActives(){
+    public function getAllMomsActives()
+    {
         $result = null;
 
         $stmt = $this->conn->prepare("SELECT t1.id_family, t1.family_code, CONCAT(t2.lastname, ' ', t2.name) AS mother_name, t2.mail, t2.cell_phone, t2.landline,
@@ -109,7 +114,7 @@ class Families extends DataConn {
                                       WHERE t1.status = 1");
 
         $stmt->execute();
-                
+
         while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
             $result[] = $row;
         }
@@ -117,10 +122,11 @@ class Families extends DataConn {
         return $result;
     }
 
-    function getAllStudentsActives(){
+    public function getAllStudentsActives()
+    {
         $result = null;
 
-        $stmt = $this->conn->prepare("SELECT t5.id_family, t5.family_code, t1.id_student, t1.student_code, CONCAT(t1.name,' ', t1.lastname) AS nombre, t1.status, 
+        $stmt = $this->conn->prepare("SELECT t5.id_family, t5.family_code, t1.id_student, t1.student_code, CONCAT(t1.name,' ', t1.lastname) AS nombre, t1.status,
                                              IF(t1.gender = 0, 'Hombre', 'Mujer') AS sexo, t2.group_code, t3.campus_name, t4.degree,
                                              CASE
                                                 WHEN t2.id_section = 1 THEN 'hombres'
@@ -135,7 +141,7 @@ class Families extends DataConn {
                                       WHERE t1.status = 1 AND t5.status = 1");
 
         $stmt->execute();
-                
+
         while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
             $result[] = $row;
         }
@@ -143,17 +149,18 @@ class Families extends DataConn {
         return $result;
     }
 
-    function getRouteSupervisors(){
+    public function getRouteSupervisors()
+    {
         $result = null;
 
         $stmt = $this->conn->prepare("SELECT t1.no_colaborador AS collaborator_number, CONCAT(t1.apellido_paterno_colaborador, ' ', t1.apellido_materno_colaborador, ' ', t1.nombres_colaborador) AS collaborator_name,
-                                             t1.correo_institucional AS institutional_mail, t2.access_password AS password
+                                             t1.correo_institucional AS institutional_mail, t1.collab_institutional_mail_hash AS mail_institutional, t2.access_password AS password
                                     FROM colaboradores_ykt.colaboradores AS t1
                                     INNER JOIN colaboradores_ykt.collaborator_access t2 ON t1.no_colaborador = t2.no_colaborador
                                     WHERE t1.status = 1 AND t1.id_area_colaborador = 5");
 
         $stmt->execute();
-                
+
         while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
             $result[] = $row;
         }
@@ -161,16 +168,17 @@ class Families extends DataConn {
         return $result;
     }
 
-    function getDebtorFamilies(){
+    public function getDebtorFamilies()
+    {
         $result = null;
 
         $stmt = $this->conn->prepare("SELECT t1.id_family, t1.family_code, t1.family_name
                                       FROM families_ykt.families AS t1
-                                      INNER JOIN families_ykt.debtor_families AS t2 ON t1.id_family = t2.id_family 
+                                      INNER JOIN families_ykt.debtor_families AS t2 ON t1.id_family = t2.id_family
                                       WHERE t1.status = 1 AND t2.transport_access = 0");
 
         $stmt->execute();
-                
+
         while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
             $result[] = $row;
         }
@@ -178,4 +186,3 @@ class Families extends DataConn {
         return $result;
     }
 }
-?>
